8c5a582df95cdd220b9c960efbc4af726c9677cd
Vinayak Joshi <vinayakj009@gmail.com>
2025-03-08 02:59:55 +0530

    updated main code to run tests

--- redisson/src/main/java/org/redisson/Main.java
+++ redisson/src/main/java/org/redisson/Main.java
@@ -5,7 +5,14 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,11 +22,106 @@ public class Main {
 
         Thread queueProcessorThread = new Thread(() -> processQueue(blockingQueue));
         queueProcessorThread.start();
-
-        // processCommands(delayedQueue);
-        runCancellationTest(delayedQueue, args);
+        // runCancellationTest(delayedQueue, args);
+        runTests(delayedQueue);
 
         cleanUp(delayedQueue, redisson);
+    }
+
+    private static void runTests(RDelayedQueue<String> delayedQueue) {
+        runRetainAllTest(delayedQueue);
+        // runRemoveAllTest(delayedQueue);
+    }
+
+    private static void runRemoveAllTest(RDelayedQueue<String> delayedQueue){
+        delayedQueue.clear();
+        List<String> randomStrings = generateRandomStrings(1000, 10);
+        List<Long> randomDelays = generateRandomDelayList(1000, 3600, 1000);
+        Map<String, Long> idToDelay = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            delayedQueue.offer(randomStrings.get(i), randomDelays.get(i), TimeUnit.SECONDS);
+            idToDelay.put(randomStrings.get(i), randomDelays.get(i));
+        }
+        List<String> selectedIds = selectFromList(randomStrings, 500);
+        delayedQueue.removeAll(selectedIds);
+        Set<String> remainingIds = new HashSet<>(delayedQueue.readAll());
+        Set<String> expectedToBeRemoved = new HashSet<>(selectedIds);
+        Set<String> expectedToBeRetained = new HashSet<>(randomStrings);
+        Set<String> testSet = new HashSet<>(expectedToBeRemoved);
+        testSet.retainAll(remainingIds);
+        if (!testSet.isEmpty()) {
+            System.out.println("runRemoveAllTest Error: Some IDs that should have been removed are still presentwith count "+ testSet.size() +" : " + testSet);
+        }
+        testSet = new HashSet<>(expectedToBeRetained);
+        testSet.removeAll(remainingIds);
+        if (!testSet.isEmpty()) {
+            System.out.println("runRemoveAllTest Error: Some IDs that should have been retained are not presentwith count "+ testSet.size() +" : " + testSet);
+        }
+    }
+
+    private static void runRetainAllTest(RDelayedQueue<String> delayedQueue){
+        delayedQueue.clear();
+        List<String> randomStrings = generateRandomStrings(1000, 10);
+        List<Long> randomDelays = generateRandomDelayList(1000, 3600, 1000);
+        Map<String, Long> idToDelay = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            delayedQueue.offer(randomStrings.get(i), randomDelays.get(i), TimeUnit.SECONDS);
+            idToDelay.put(randomStrings.get(i), randomDelays.get(i));
+        }
+        List<String> selectedIds = selectFromList(randomStrings, 500);
+        // delayedQueue.retainAll(selectedIds);
+        Set<String> remainingIds = new HashSet<>(delayedQueue.readAll());
+        Set<String> expectedToBeRetained = new HashSet<>(selectedIds);
+        Set<String> expectedToBeRemoved = new HashSet<>(randomStrings);
+        expectedToBeRemoved.removeAll(expectedToBeRetained);
+
+        Set<String> testSet = new HashSet<>(expectedToBeRemoved);
+        testSet.retainAll(remainingIds);
+        if (!testSet.isEmpty()) {
+            System.out.println("runRetainAllTest Error: Some IDs that should have been removed are still presentwith count "+ testSet.size() +" : " + testSet);
+        }
+        testSet = new HashSet<>(expectedToBeRetained);
+        testSet.removeAll(remainingIds);
+        if (!testSet.isEmpty()) {
+            System.out.println("runRetainAllTest Error: Some IDs that should have been retained are not present with count "+ testSet.size() +" : " + testSet);
+        }
+    }
+
+    private static <V> List<V> selectFromList(List<V> list, int count) {
+        List<V> selected = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            selected.add(list.get(random.nextInt(list.size())));
+        }
+        return selected;
+    }
+
+    private static List<Long> generateRandomDelayList(int count, long maxDelay, long minDelay) {
+        List<Long> randomDelays = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            randomDelays.add(((long) random.nextInt((int) maxDelay)) + minDelay);
+        }
+        return randomDelays;
+    }
+
+    private static List<String> generateRandomStrings(int count, int length) {
+        List<String> randomStrings = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            randomStrings.add(generateRandomString(random, length) + i);
+        }
+        return randomStrings;
+    }
+
+
+    private static String generateRandomString(Random random, int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
     }
 
     private static RedissonClient configureRedisson() {
